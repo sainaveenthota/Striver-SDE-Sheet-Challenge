f62f061261f95aa239da60d018e1b5346a9c7b7c
sainaveenthota <67990986+sainaveenthota@users.noreply.github.com>
2022-07-06 09:29:32 +0530

    Update 6. Stock buy and Sell.java

--- 1. Arrays/6. Stock buy and Sell.java
+++ 1. Arrays/6. Stock buy and Sell.java
@@ -1,1 +1,10 @@
-
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0, min = prices[0], n = prices.length;
+        for(int i=0;i<n;i++){
+            min = Math.min(min, prices[i]);
+            profit = Math.max(profit,prices[i]-min);
+        }
+        return profit;
+    }
+}
