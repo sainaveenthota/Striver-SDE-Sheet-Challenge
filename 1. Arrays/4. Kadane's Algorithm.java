971668b979f75caaa5e4d3db5993cc4dc6c9ce39
sainaveenthota <67990986+sainaveenthota@users.noreply.github.com>
2022-07-06 09:24:19 +0530

    Update 4. Kadane's Algorithm.java

--- 1. Arrays/4. Kadane's Algorithm.java
+++ 1. Arrays/4. Kadane's Algorithm.java
@@ -1,1 +1,11 @@
-
+class Solution {
+    public int maxSubArray(int[] nums) {// Kadane's algo
+        int n = nums.length, max = nums[0], sum = 0;
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+            max = Math.max(sum,max);
+            if(sum<0) sum = 0;
+        }
+        return max;
+    }
+}
