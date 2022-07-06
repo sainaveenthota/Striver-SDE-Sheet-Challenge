7e30a4570d209e3abe952ce11e66f8382e092452
sainaveenthota <67990986+sainaveenthota@users.noreply.github.com>
2022-07-06 09:27:29 +0530

    Update 5. Sort an array of 0's 1's 2's.java

--- 1. Arrays/5. Sort an array of 0's 1's 2's.java
+++ 1. Arrays/5. Sort an array of 0's 1's 2's.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                mid++;low++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+    }
+}
